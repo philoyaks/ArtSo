b1514002a963ac60a669a26074e5200fb58a5250
Oyaleke Philip <oyaleke.philip@gmail.com>
2021-10-19 18:34:45 +0100

    first

---
+++ Pages/Profile/Edit_profile_page.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Atso.Pages.Profile
+{
+    public class Edit_profile_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
